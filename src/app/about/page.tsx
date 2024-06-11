9d65a0680628c1721d2b79db456fefab572d0e80
Subarna Bhandari <subarnabhd@Subarnas-MacBook-Pro.local>
2024-06-11 10:49:10 +0545

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
     <main className='bg-black'>
 
 
-    <div className="container flex flex-col m-auto bg-neutral-4 rounded-2xl py-5 gap-10 pb-20">
+      <div className="container flex flex-col m-auto bg-neutral-4 rounded-2xl py-5 gap-10 pb-20">
         <Image
           className="m-auto w-full rounded-xl"
           src="/about/about-header.jpg"
@@ -18,25 +18,25 @@ const page = () => {
           height={100}
           alt=""
         />
-<div className='flex flex-col gap-3'>
-        <h2 className="text-4xl color-primary-600 font-bold pb-2 ">
-        Hello!👋I’m Subarna, a Web, Graphic, & UI/UX Designer.
-        </h2>
-        <p className=" text-base mb-4 color-white90">
-        Based in Nepal, 👨‍💻I help business owners and entrepreneurs create their brand identities, have an online presence, and turn their businesses into the next best thing in the market! Design for me more than just digital applications. I am a painter, a crafter, a photographer, and love decorating spaces. It wasn’t a feasible business plan but it was a creative outlet for me and I loved it! In 2015, I started Designing and never looked back. Now, in my 5 years, I have worked with clients from all genres across the globe and it’s been an amazing journey. I am always looking forward to the opportunities, both online/freelance and in-house, to learn and flex my creative skills towards building an impactful brand.
-       </p>
-       <p className=" text-base mb-4 color-white90">When I’m not glued to the computer, I like to spend time watching movies, painting. I am also an adrenaline junkie and love all sports above and below ground level – doesn’t mean I’m necessarily good at them all!</p>
-        <p className='text-bold'>Creating the Brand, that people <b>Love.</b></p>
+        <div className='flex flex-col gap-3'>
+          <h2 className="text-4xl color-primary-600 font-bold pb-2 ">
+            Hello!👋I’m Subarna, a Web, Graphic, & UI/UX Designer.
+          </h2>
+          <p className=" text-base mb-4 color-white90">
+            Based in Nepal, 👨‍💻I help business owners and entrepreneurs create their brand identities, have an online presence, and turn their businesses into the next best thing in the market! Design for me more than just digital applications. I am a painter, a crafter, a photographer, and love decorating spaces. It wasn’t a feasible business plan but it was a creative outlet for me and I loved it! In 2015, I started Designing and never looked back. Now, in my 5 years, I have worked with clients from all genres across the globe and it’s been an amazing journey. I am always looking forward to the opportunities, both online/freelance and in-house, to learn and flex my creative skills towards building an impactful brand.
+          </p>
+          <p className=" text-base mb-4 color-white90">When I’m not glued to the computer, I like to spend time watching movies, painting. I am also an adrenaline junkie and love all sports above and below ground level – doesn’t mean I’m necessarily good at them all!</p>
+          <p className='text-bold'>Creating the Brand, that people <b>Love.</b></p>
         </div>
         <div className="flex gap-5">
           <Link className="primary-btn" href="/contact">Let‘s Connect</Link>
 
           <Link className="border-btn" href="/cv">View Resume</Link>
         </div>
-    </div>
+      </div>
 
 
-  </main>
+    </main>
   )
 }
 
--- src/components/studio/Studiobanner.tsx
+++ src/components/studio/Studiobanner.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Studiobanner = () => {
   return (
-    <div className="studio-banner h-screen">
+    <div className="studio-banner h-screen ">
+      <div className='absolute top-[15%] left-1/2 transform -translate-x-1/2 flex flex-col gap-4 items-center m-auto'> 
+      <Image
+          className=" m-auto  text-right"
+          src="/studio/tss.svg"
+          width={100}
+          height={100}
+          alt=""
+        />
+        <h2 className='text-4xl font-bold  text-center'>The Subarna Studio</h2>
+        <h2 className='text-base font-normal text-center color-white90'>studio.subarnabhandari.com</h2>
+      </div>
         
     </div>
   )
