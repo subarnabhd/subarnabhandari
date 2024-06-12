6996e7a677915f5740262b12015d5b353c618ae0
Subarna Bhandari <subarnabhd@Subarnas-MacBook-Pro.local>
2024-06-13 01:14:59 +0545

    update

--- src/app/(.com)/preeti/page.tsx
+++ src/app/(.com)/preeti/page.tsx
@@ -6,13 +6,13 @@ const page = () => {
   return (
     <div className='p-10'>
 
-    <div className=" unicode container m-auto align-middle text-center text-white  justify-center  flex flex-col border border-gray-950 p-10 w-5/12  rounded-3xl ">
+    <div className=" unicode container m-auto align-middle text-center text-white  justify-center  flex flex-col border border-gray-900 p-10 w-5/12  rounded-3xl ">
         <div className="flex gap-4 justify-center items-center    py-2">
             <Link className="" href="/unicode"> 
             <Image className='m-auto w-max' src="/tools/conv.webp" width={50} height={50} alt=''></Image></Link>
             <div className='text-left'>
                 <p className=' font-light'>Preeti to Unicode</p>
-                <p className='text-2xl font-semibold'>Converter</p>
+                <p className='text-[21px] leading-6 font-semibold uppercase'>Converter</p>
             </div>
         </div>
        
