2d82bc77c7116bd035123c3745766c013b7f0e32
Subarna Bhandari <subarnabhd@Subarnas-MacBook-Pro.local>
2024-06-13 00:23:46 +0545

    update

--- src/components/home/Hire.tsx
+++ src/components/home/Hire.tsx
@@ -6,7 +6,7 @@ const Hire = () => {
     return (
         <div className="py-20 w-header">
             <div className=" container m-auto bg-[#F5F5F7] rounded-2xl flex  ">
-                <div className=' w-1/2  color-black70  items-center flex flex-col text-center justify-center'>
+                <div className=' w-7/12  color-black70  items-center flex flex-col text-center justify-center'>
              
                         <h1 className='font-bold text-5xl text-black'>Got an idea?</h1>
                         <h1 className='font-bold text-5xl pb-5 text-black'>Let's work together.</h1>
@@ -19,8 +19,8 @@ const Hire = () => {
        
                    
                 </div>
-                <div className="w-1/2">
-                    <Image className="w-full" src="/home/subarnabhd-showcase.webp" alt="Subarna Bhandari"  width={2000} height={100}></Image>
+                <div className="w-6/12">
+                    <Image className="w-full" src="/home/subarnabhd-showcase.webp" alt="Subarna Bhandari"  width={3000} height={100}></Image>
                 </div>
             </div>
 
