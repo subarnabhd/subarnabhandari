a09bb9f9e1f169688ace9904b9a6236cad84337d
Subarna Bhandari <subarnabhd@Subarnas-MacBook-Pro.local>
2024-06-13 01:05:58 +0545

    update

--- src/app/(.com)/preeti/page.tsx
+++ src/app/(.com)/preeti/page.tsx
@@ -9,16 +9,16 @@ const page = () => {
     <div className=" unicode container m-auto align-middle text-center text-white  justify-center  flex flex-col border border-gray-950 p-10 w-5/12  rounded-3xl ">
         <div className="flex gap-4 justify-center items-center    py-2">
             <Link className="" href="/unicode"> 
-            <Image className='m-auto w-max' src="/tools/conv.webp" width={50} height={50}></Image></Link>
+            <Image className='m-auto w-max' src="/tools/conv.webp" width={50} height={50} alt=''></Image></Link>
             <div className='text-left'>
                 <p className=' font-light'>Preeti to Unicode</p>
                 <p className='text-2xl font-semibold'>Converter</p>
             </div>
         </div>
        
-        <iframe className="frame h-[380px]" src="https://www.ashesh.com.np/unicode-preeti/linkapi.php?api=150059o468" frameborder="0"
-            scrolling="no" marginwidth="0" marginheight="0"
-            allowtransparency="true">
+        <iframe className="frame h-[380px]" src="https://www.ashesh.com.np/unicode-preeti/linkapi.php?api=150059o468"  >
+        
+ 
           </iframe>
     </div>
 </div>
--- src/app/(.com)/unicode/page.tsx
+++ src/app/(.com)/unicode/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
             <div className=" unicode container m-auto align-middle text-center text-white  justify-center  flex flex-col border border-gray-950 p-10 w-5/12  rounded-3xl ">
                 <div className="flex gap-4 justify-center items-center    py-2">
                     <Link className="" href="/preeti"> 
-                    <Image className='m-auto w-max' src="/tools/conv.webp" width={50} height={50}></Image></Link>
+                    <Image className='m-auto w-max' src="/tools/conv.webp" width={50} height={50} alt=''></Image></Link>
                     <div className='text-left'>
                         <p className=' font-light'>Unicode to Preeti</p>
                         <p className='text-2xl font-semibold'>Converter</p>
