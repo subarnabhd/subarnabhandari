150bf8d4d21c624fbd40b55b703feb2ffd82df11
Subarna Bhandari <subarnabhd@Subarnas-MacBook-Pro.local>
2024-06-13 00:20:58 +0545

    update

--- src/components/home/Hire.tsx
+++ src/components/home/Hire.tsx
@@ -8,8 +8,8 @@ const Hire = () => {
             <div className=" container m-auto bg-[#F5F5F7] rounded-2xl flex  ">
                 <div className=' w-1/2  color-black70  items-center flex flex-col text-center justify-center'>
              
-                        <h1 className='font-bold text-4xl text-black'>Got an idea?</h1>
-                        <h1 className='font-bold text-4xl pb-5 text-black'>Let's work together.</h1>
+                        <h1 className='font-bold text-5xl text-black'>Got an idea?</h1>
+                        <h1 className='font-bold text-5xl pb-5 text-black'>Let's work together.</h1>
                         <p>Are you an entrepreneur/business with a great idea,</p>
                         <p>but need an identity to start with?</p>
                         <p>I love ideas because I believe in them.</p>
@@ -20,7 +20,7 @@ const Hire = () => {
                    
                 </div>
                 <div className="w-1/2">
-                    <Image className="w-min" src="/home/subarnabhd-showcase.webp" alt="Subarna Bhandari"  width={1500} height={100}></Image>
+                    <Image className="w-full" src="/home/subarnabhd-showcase.webp" alt="Subarna Bhandari"  width={2000} height={100}></Image>
                 </div>
             </div>
 
