69462e559e92ebb5ceb2fb0157dfa22c918913b1
Subarna Bhandari <subarnabhd@Subarnas-MacBook-Pro.local>
2024-06-13 09:57:45 +0545

    update

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <main className='text-white bg-black font-sm'>
+            <div className="container py-20 m-auto flex flex-col gap-5">
+                <p className='font-bold text-2xl'> Privacy Policy</p>
+                <p>Your privacy is important to us. It is Subarna Bhandari's policy to respect your privacy regarding any information we may collect from you across our website, subarnabhandari.com, and other sites we own and operate.</p>
+
+                    <p>Information We Collect</p>
+                    <p>We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.
+                    </p>
+                    <p>Contact Us</p>
+                    <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at subarnabhd@gmail.com.</p>
+            </div>
+        </main>
+    )
+}
+
+export default page
