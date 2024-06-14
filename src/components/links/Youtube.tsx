cd9cff1ac3837157af6382b4435147546b2168f5
Subarna Bhandari <subarnabhd@Subarnas-MacBook-Pro.local>
2024-06-14 13:30:30 +0545

    update

--- src/components/links/Youtube.tsx
+++ src/components/links/Youtube.tsx
@@ -2,49 +2,58 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Instagram = () => {
+const Youtube = () => {
+  const channels = [
+    {
+      href: "https://www.youtube.com/@subarnabhd",
+      imgSrc: "/about/subarnabhandari.jpg",
+      imgAlt: "Subarnabhd",
+      name: "Subarnabhd",
+      handle: "@subarnabhd",
+      channelId: "UCaLuSXtqYkLNbqKpF30kMEA"
+    },
+    {
+      href: "https://www.youtube.com/@thesubarnastudio",
+      imgSrc: "/studio/thesubarnastudio.png",
+      imgAlt: "The Subarna Studio",
+      name: "The Subarna Studio",
+      handle: "@thesubarnastudio",
+      channelId: "UCHAv0rdJ7IKkdH4XL4A3QCA"
+    },
+    {
+      href: "https://www.youtube.com/@memoriesline",
+      imgSrc: "/link/memoriesline.jpg",
+      imgAlt: "Memoriesline",
+      name: "Memoriesline",
+      handle: "@memoriesline",
+      channelId: "UCkz2T5GUlxJfleZOkJFSVDQ"
+    }
+  ];
+
   return (
-    <div className="Instagram py-20 bg-white w-header">
-      <div className="container m-auto flex flex-col gap-10">
-        <div className="text-center m-auto">
-          <Image className='m-auto' src="/link/instagram.webp" height={60} width={60} alt="Instagram Logo" />
-          <h2 className='text-2xl font-bold text-center text-black'>Follow us on Instagram</h2>
-          <p className='text-sm color-black80 text-center'>Show some love on our Instagram page</p>
+    <div className="Youtube-link py-20">
+      <div className="container m-auto flex gap-5">
+        <div className="flex flex-col justify-center items-center gap-4 p-10">
+          <Image className='p-0' src="/link/youtube.webp" alt="YouTube Logo" height={60} width={60} />
+          <h2 className='text-3xl font-bold text-center'>Subscribe us on Youtube</h2>
+          <p className='text-sm color-white80 text-center'>Subscribe Now and press the bell button</p>
         </div>
-        <div className="flex gap-10 justify-center w-full">
-          <Link className="ibutton lbox col" href="https://www.instagram.com/subarnabhd" target="_blank">
-            <div className="flex gap-3 items-center shadow-medium w-max rounded-full p-5">
-              <Image className='rounded-full border' src="/about/subarnabhandari.jpg" height={60} width={60} alt="Subarnabhd Profile" />
-              <div className="itext">
-                <p className="link-1 text-black font-bold">Subarnabhd</p>
-                <p className="link-1 text-black text-sm">@subarnabhd</p>
-              </div>
-            </div>
-          </Link>
 
-          <Link className="ibutton lbox col" href="https://www.instagram.com/thesubarnastudio" target="_blank">
-            <div className="flex gap-3 items-center shadow-medium w-max rounded-full p-5">
-              <Image className='rounded-full border' src="/studio/thesubarnastudio.png" height={60} width={60} alt="The Subarna Studio Profile" />
-              <div className="itext">
-                <p className="link-1 text-black font-bold">The Subarna Studio</p>
-                <p className="link-1 text-black text-sm">@thesubarnastudio</p>
+        <div className="w-8/12 flex gap-5 items-center justify-center">
+          {channels.map((channel, index) => (
+            <Link key={index} className="link-box" href={channel.href} target="_blank">
+              <div className="cbutton">
+                <Image src={channel.imgSrc} alt={channel.imgAlt} height={100} width={100} />
+                <p className="link-1">{channel.name}</p>
+                <p className="link-2">{channel.handle}</p>
+                <div className="g-ytsubscribe" data-channelid={channel.channelId} data-layout="default" data-count="default"></div>
               </div>
-            </div>
-          </Link>
-
-          <Link className="ibutton lbox col" href="https://www.instagram.com/sliceblaze/" target="_blank">
-            <div className="flex gap-3 items-center shadow-medium w-max rounded-full p-5">
-              <Image className='rounded-full' src="/link/sliceblaze.jpg" height={60} width={60} alt="Sliceblaze Profile" />
-              <div className="itext">
-                <p className="link-1 text-black font-bold">Sliceblaze</p>
-                <p className="link-1 text-black text-sm">@sliceblaze</p>
-              </div>
-            </div>
-          </Link>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Instagram
+export default Youtube;
