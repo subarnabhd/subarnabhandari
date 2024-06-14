0315b29c4b5e0d4fb7feeb357861acb19a3e4c45
Subarna Bhandari <subarnabhd@Subarnas-MacBook-Pro.local>
2024-06-14 22:31:55 +0545

    update

---
+++ src/app/(pay)/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "/style/pay.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Subarna Bhandari",
+  description: "Designer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+          {children}
+      </body>
+    </html>
+  );
+}
